b2bd6467672c2a39f38106657efa7a6d6e22e06d
Ankit Kumar <krankit01012003@gmail.com>
2025-02-07 16:44:25 +0530

    hello

--- App.jsx
+++ App.jsx
@@ -1,15 +1,34 @@
-import { View, Text, Button} from 'react-native'
-import React , {useState} from 'react'
-import Student from './components/Student'
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 
 const App = () => {
-  const [show, setShow] = useState(false);
+  const [data] = useState([
+    { id: 1, lang: "PHP" },
+    { id: 2, lang: "JAVA" },
+    { id: 3, lang: "JavaScript" },
+    { id: 4, lang: "Swift" },
+  ])
+
+  const [currActive, setCurrActive] = useState(1)
 
   return (
     <View>
-      <Text>UseEffect for unmount component: {show ? "Visible" : "Hidden"}</Text>
-      <Button onPress={()=>setShow(!show)} title='Show/Hide'/>
-       { show && <Student/>}
+
+      <View style={{ width: '100%', height: "100%", margin: 10 }}>
+
+        {
+          data.map((item, index) => (
+            <TouchableOpacity key={index} style={{ flexDirection: 'row', alignItems: 'center', }}
+              onPress={() => setCurrActive(item.id)}
+            >
+              {
+                currActive === item.id ? <View style={{ width: 20, height: 20, backgroundColor: 'green', borderColor: 'red', borderRadius: 10, margin: 5 }}></View> : <View style={{ width: 20, height: 20, backgroundColor: 'green', borderColor: 'red', margin: 5 }}></View>
+              }
+              <Text>{item.lang}</Text>
+            </TouchableOpacity>
+          ))
+        }
+      </View>
     </View>
   )
 }
