2d2647683f2073773c19a499717dd5e1a956ce0e
Ankit Kumar <krankit01012003@gmail.com>
2025-02-07 10:07:53 +0530

    class

--- App.jsx
+++ App.jsx
@@ -1,87 +1,20 @@
-import { View, Text, FlatList, Image, ScrollView, StyleSheet } from "react-native";
-import React, { useState } from "react";
-import dummyData from "./dummyData.json";
+import React , {Component} from "react";
+import Student from './components/Student';
+import { View ,Text,Button} from "react-native";
 
-const App = () => {
-  const [data] = useState(dummyData);
-
-  return (
-    <ScrollView nestedScrollEnabled={true}>
+class App extends Component{
+  fruit = ()=>{
+    console.log("Apple");
+  }
+  render(){
+    return(
       <View>
-        <Text style={{ margin: 10 }}>I am using FlatList with horizontal</Text>
-        <FlatList
-          style={{ padding: 10 }}
-          horizontal
-          data={data}
-          renderItem={({ item }) => (
-            <View style={styles.listItem}>
-              <Image style={styles.largeImage} source={{ uri: item.image }} />
-              <Text style={styles.nameText}>{item.name}</Text>
-              <Text>{item.email}</Text>
-            </View>
-          )}
-          keyExtractor={(item) => item.id.toString()}
-        />
+        <Text>App Class Components</Text>
+        <Button title={"Press"} onPress={()=>this.fruit()}></Button>
+        <Student/>
       </View>
+    )
+  }
+}
 
-      <Text style={{ margin: 10 }}>I am using map</Text>
-      <View>
-        {data.map((item) => (
-          <View key={item.id} style={styles.imageContainer}>
-            <Image source={{ uri: item.image }} style={styles.image} />
-          </View>
-        ))}
-      </View>
-
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.id.toString()}
-        numColumns={2} // Set number of columns
-        renderItem={({ item }) => (
-          <View style={styles.gridItem}>
-            <Image source={{ uri: item.image }} style={styles.image} />
-          </View>
-        )}
-      />
-    </ScrollView>
-  );
-};
-
-export default App;
-
-const styles = StyleSheet.create({
-  listItem: {
-    padding: 15,
-    backgroundColor: "grey",
-    margin: 10,
-    borderRadius: 5,
-    alignItems: "center",
-  },
-  largeImage: {
-    width: 200,
-    height: 200,
-    borderRadius: 150,
-  },
-  nameText: {
-    fontSize: 20,
-    fontStyle: "italic",
-    fontWeight: "bold",
-  },
-  imageContainer: {
-    padding: 15,
-    backgroundColor: "grey",
-    margin: 10,
-    borderRadius: 5,
-    alignItems: "center",
-  },
-  image: {
-    width: 100,
-    height: 100,
-    borderRadius: 5,
-  },
-  gridItem: {
-    flex: 1,
-    margin: 10,
-    alignItems: "center",
-  },
-});
+export default App
---
+++ components/Student.jsx
@@ -0,0 +1,15 @@
+import React , {Component} from "react";
+import { View ,Text} from "react-native";
+
+class Student extends Component{
+
+  render(){
+    return(
+      <View>
+        <Text>Student Class Components</Text>
+      </View>
+    )
+  }
+}
+
+export default Student
