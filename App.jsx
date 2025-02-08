931ce39ff79257a1001c74a1fdeee8ae927018d2
Ankit Kumar <krankit01012003@gmail.com>
2025-02-08 10:14:34 +0530

    not work b/c of cors policy

--- App.jsx
+++ App.jsx
@@ -1,10 +1,40 @@
-import { View, Text } from 'react-native'
+import { View, Text,Button, Alert } from 'react-native'
 import React from 'react'
 import { WebView } from "react-native-webview";
 
 const App = () => {
+  const saveAPIData = async ()=>{
+    console.log("1");
+    const data = {
+        "name": "kanak",
+        "age": 24,
+        "email": "kanak@gmail.com"
+    }
+    const url = 'http://localhost:3000/users'
+    try {
+      let response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" ,"Accept": "application/json"},
+        body: JSON.stringify(data),
+      });
+    
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+    
+      let result = await response.json(); // Ensure response is properly parsed
+      console.log(result);
+      Alert.alert(result);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+    
+  }
   return (
-     <WebView source={{uri:"https://reactnative.dev/"}}/>
+     <View>
+      <Text style={{fontSize:30}}>Post API Call</Text>
+      <Button title='Save Data' onPress={saveAPIData}></Button>
+     </View>
   )
 }
 
